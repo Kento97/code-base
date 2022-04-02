e32abf8ece30af7ba5366e4323aa6357b5eaa65b
Human Town <ty.1997@foxmail.com>
2022-04-02 11:06:43 +0800

    新增本地存储方法

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 <img class="lyb" src="https://avatars.githubusercontent.com/u/55472948?v=4" style="width:200px;margin:0 auto;border-radius:50%" />
+
+# 原生封装
+
+## 本地存储
+
+> 永久存储
+```js
+$storage.set(key, value); //设置值
+$storage.get(key); //获取值
+$storage.del(key); //删除指定值
+$storage.clear(); //清空所有数据
+```
---
+++ kento.js
@@ -0,0 +1,17 @@
+/* 原生 */
+
+//本地存储
+export const $storage = {
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  get(key) {
+    return JSON.parse(localStorage.getItem(key));
+  },
+  del(key) {
+    localStorage.removeItem(key);
+  },
+  clear() {
+    localStorage.clear();
+  },
+};
